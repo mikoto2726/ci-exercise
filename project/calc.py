8656256ecaad3988e1098898a3a7d81202c8f500
1f10220137 <s1f102201379@iniad.org>
2023-06-22 14:23:45 +0900

    テストメソッドを追加

--- project/calc.py
+++ project/calc.py
@@ -1,7 +1,7 @@
 def fact(n):
     if n < 0:
         raise ValueError("n must be a non-negative integer")
-    if n = 0:
+    if n == 0:
         return 1
     result = 1
     for i in range(1, n+1):
