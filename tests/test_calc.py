0c53aa1ef6c3fd75edf78471bf5db746bf8f6f2a
1f10220137 <s1f102201379@iniad.org>
2023-06-22 14:31:38 +0900

    add gcb

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -13,3 +13,11 @@ class TestTarget(unittest.TestCase):
     def test_fact_negative(self):
         with self.assertRaises(ValueError):
             fact(-1)
+
+    def test_gcb(self):
+        self.assertEqual(gcb(12, 8), 4)
+        self.assertEqual(gcb(14, 28), 14)
+        self.assertEqual(gcb(21, 14), 7)
+        self.assertEqual(gcb(56, 48), 8)
+        self.assertEqual(gcb(77, 42), 7)
+
